c77062aa788dec9d9eeaf6062ffb28480acaf236
carlostorralba12 <carlostorralba12@gmail.com>
2020-12-19 17:00:24 +0100

    Añado idioma español al proyecto

--- web_farmacia/database/seeders/UsersTableSeeder.php
+++ web_farmacia/database/seeders/UsersTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -18,10 +19,17 @@ class UsersTableSeeder extends Seeder
         $user = new User();
         $user->name = 'admin';
         $user->email = 'admin@admin';
-        $user->password = 'admin';
+        $user->password = Hash::make('admin');
         $user->dni = '12345678H';
         $user->adress = 'C/Admin';
-
         $user->save();
+        // Añadimos una entrada a esta tabla
+        /*User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin',
+            'dni' => '12345678H',
+            'adress' => 'C/Admin',
+            'password' => Hash::make('admin'),
+        ]);*/
     }
 }
