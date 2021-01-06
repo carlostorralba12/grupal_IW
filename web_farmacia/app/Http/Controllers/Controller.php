f54a699fc8513e62dfc1196b2f82d7f99674c77b
carlostorralba12 <carlostorralba12@gmail.com>
2021-01-06 14:50:33 +0100

    Añado cambios necesarios

--- web_farmacia/app/Http/Controllers/Controller.php
+++ web_farmacia/app/Http/Controllers/Controller.php
@@ -10,9 +10,5 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-    public function showCatalogo(){
-        $producto=Producto::paginate(5);
-        
-        return view('catalogo', ['productos' => $producto]);
-    }
+    
 }
