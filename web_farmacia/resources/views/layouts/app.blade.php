2f0d04a32c17114b45305370faa4d5c543f3d404
Gema <glj4@alu.ua.es>
2021-01-19 01:28:26 +0100

    Página de inicio creada

--- web_farmacia/resources/views/layouts/app.blade.php
+++ web_farmacia/resources/views/layouts/app.blade.php
@@ -33,7 +33,8 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <a class="navbar-brand" href="{{ url('/about') }}">About</a>
+                        <a class="navbar-brand" href="{{ url('/contacto') }}">Contact Us</a>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
--- web_farmacia/routes/web.php
+++ web_farmacia/routes/web.php
@@ -25,6 +25,14 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contacto', function () {
+    return view('contacto');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
